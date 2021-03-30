07c255ab834fe212f3f99b1d0608eb389dba9743
Stephen Josef Wissow <sjw1000@wildcats.unh.edu>
2021-03-30 10:50:48 -0400

    ml: generate more ideas

--- src/generate_data.py
+++ src/generate_data.py
@@ -16,8 +16,34 @@ Python 3, please.
 from asv_sim.dynamics import Dynamics
 from asv_sim.cw4 import cw4
 
+class Generator():
+    def __init__(self):
+        self.boat = Dynamics(cw4)
+
+    def generate(self, filename):
+        """
+        Reads control file, runs simulation accordingly, and returns data.
+
+        :param filename: for file containing control inputs
+        """
+
+# TODO create my own callback function
+# TODO import rospy.Timer
+"""
+basically, I'll start a Timer, pass it my own callback.
+
+my own callback will grab the TimerEvent instance that it's passed,
+will extract the time and pass that to Dynamics.update() and pass the next set of controls.
+
+I probably want to create my own object that eats an input file of commands,
+and then steps through them each time its callback is passed.
+
+I'll need to capture the data somehow, too..maybe I'll be subscribing to topics? So, there's no ROS node running...I think I'll actually just be checking the state of the Dynamics object, and recording that.
+"""
+
 def main():
-    mine = Dynamics(cw4)
+    print("Hello, world!")
+    pass
 
 if __name__ == '__main__':
     main()
